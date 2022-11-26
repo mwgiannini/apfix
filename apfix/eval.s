6eaeed60bd81022e936ec416e75d2bced22934c1
mwgiannini <89871491+mwgiannini@users.noreply.github.com>
2022-11-26 14:54:50 -0500

    Add test stub

---
+++ apfix/eval.s
@@ -0,0 +1,21 @@
+#
+#  eval.s
+#  apfix
+#
+#  Created by Amor Wang & MW Giannini on 11/26/22.
+#
+
+.data
+
+.bss
+
+.text
+.global _evalPostfix
+_evalPostfix:
+
+
+12.5 + 56.4
+12.5,56.4,+,45.67,
+
+ret
+.end
